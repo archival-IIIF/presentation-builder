148aea29abaf89352269292104df1583ead94264
Fabian <edv@sozarch.uzh.ch>
2021-08-06 09:12:03 +0200

    npm changes

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import Annotation from './v3/Annotation';
+import AuthService from './v3/AuthService';
+import Base from './v3/Base';
+import Canvas from './v3/Canvas';
+import Collection from './v3/Collection';
+import Manifest from './v3/Manifest';
+import Resource from './v3/Resource';
+import TextResource from './v3/TextResource';
+
+import * as AnnotationV2 from './v2/Annotation';
+import * as AnnotationListV2 from './v2/AnnotationList';
+import * as AuthServiceV2 from './v2/AuthService';
+import * as BaseV2 from './v2/Base';
+import * as CanvasV2 from './v2/Canvas';
+import * as CollectionV2 from './v2/Collection';
+import * as ImageV2 from './v2/Image';
+import * as ManifestV2 from './v2/Manifest';
+import * as MediaSequenceV2 from './v2/MediaSequence';
+import * as RenderingV2 from './v2/Rendering';
+import * as SearchHitV2 from './v2/SearchHit';
+import * as SequenceV2 from './v2/Sequence';
+import * as TermListV2 from './v2/TermList';
+import * as TextResourceV2 from './v2/TextResource';
+
+
+// without default
+export {
+    Annotation,
+    AuthService,
+    Base,
+    Canvas,
+    Collection,
+    Manifest,
+    Resource,
+    TextResource,
+
+    AnnotationV2,
+    AnnotationListV2,
+    AuthServiceV2,
+    BaseV2,
+    CanvasV2,
+    CollectionV2,
+    ImageV2,
+    ManifestV2,
+    MediaSequenceV2,
+    RenderingV2,
+    SearchHitV2,
+    SequenceV2,
+    TermListV2,
+    TextResourceV2
+}
