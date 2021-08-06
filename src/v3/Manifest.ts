3030b5317148d5b539546a9212791331e9468066
Fabian <edv@sozarch.uzh.ch>
2021-08-06 10:26:29 +0200

    @context for Collection and Manifest

--- src/v3/Manifest.ts
+++ src/v3/Manifest.ts
@@ -3,5 +3,6 @@ import Base from './Base';
 export default class Manifest extends Base {
     constructor(id: string, label: string) {
         super(id, 'Manifest', label);
+        this["@context"] = "http://iiif.io/api/presentation/3/context.json";
     }
 }
