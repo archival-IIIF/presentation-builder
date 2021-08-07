e8744f868f66549ab42db937a9a1212f92b69c7c
Fabian <edv@sozarch.uzh.ch>
2021-08-07 12:57:53 +0200

    Collection and Manifest items changed

--- src/v3/Collection.ts
+++ src/v3/Collection.ts
@@ -1,8 +1,47 @@
-import Base, {Internationalize} from './Base';
+import Base, {Internationalize, Ref} from './Base';
+import Resource from "./Resource";
+import Manifest from "./Manifest";
+
+export interface ManifestRef extends Ref {
+    thumbnail?: Resource;
+}
+
+export interface CollectionRef extends Ref {
+    thumbnail?: Resource;
+}
 
 export default class Collection extends Base {
+
+    items: ManifestRef[] | CollectionRef[] | undefined;
+
     constructor(id: string, label: Internationalize) {
         super(id, 'Collection', label);
         this["@context"] = "http://iiif.io/api/presentation/3/context.json";
+    }
+
+    addCollection(collection: Collection) {
+        if (!this.items) {
+            this.items = [];
+        }
+
+        this.items.push({
+            id: collection.id,
+            type: collection.type,
+            label: collection.label,
+            thumbnail: collection.thumbnail[0]
+        })
+    }
+
+    addManifest(manifest: Manifest) {
+        if (!this.items) {
+            this.items = [];
+        }
+
+        this.items.push({
+            id: manifest.id,
+            type: manifest.type,
+            label: manifest.label,
+            thumbnail: manifest.thumbnail[0]
+        })
     }
 }
--- src/v3/Manifest.ts
+++ src/v3/Manifest.ts
@@ -1,6 +1,10 @@
 import Base, {Internationalize} from './Base';
+import Canvas from "./Canvas";
 
 export default class Manifest extends Base {
+
+    items: Canvas[] | undefined;
+
     constructor(id: string, label: Internationalize) {
         super(id, 'Manifest', label);
         this["@context"] = "http://iiif.io/api/presentation/3/context.json";
