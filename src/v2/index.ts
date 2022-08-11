dc40fa7be80ce8dace054c0f2b9ce522e4d34f37
kerim1 <kerim1@users.noreply.github.com>
2022-08-11 15:12:52 +0200

    Update dependencies and set exports per version of the IIIF spec

---
+++ src/v2/index.ts
@@ -0,0 +1,43 @@
+import Annotation, {XYWH} from './Annotation';
+import AnnotationList from './AnnotationList';
+import AuthService, {AuthTexts} from './AuthService';
+import Base, {Metadata, SeeAlso, Related, Ref} from './Base';
+import Canvas from './Canvas';
+import Collection from './Collection';
+import Image, {ImageProfile, AccessTier, Tiles} from './Image';
+import Manifest from './Manifest';
+import MediaSequence from './MediaSequence';
+import Rendering from './Rendering';
+import Resource from './Resource';
+import SearchHit from './SearchHit';
+import Sequence from './Sequence';
+import TermList from './TermList';
+import TextResource from './TextResource';
+
+export {
+    Annotation,
+    AnnotationList,
+    AuthService,
+    Base,
+    Canvas,
+    Collection,
+    Image,
+    Manifest,
+    MediaSequence,
+    Rendering,
+    Resource,
+    SearchHit,
+    Sequence,
+    TermList,
+    TextResource,
+
+    XYWH,
+    AuthTexts,
+    Metadata,
+    SeeAlso,
+    Related,
+    Ref,
+    ImageProfile,
+    AccessTier,
+    Tiles
+};
---
+++ src/v3/index.ts
@@ -0,0 +1,47 @@
+import Annotation, {TimeMode, XYWH} from './Annotation';
+import AnnotationCollection from './AnnotationCollection';
+import AnnotationPage from './AnnotationPage';
+import AuthService, {AuthTexts} from './AuthService';
+import Base, {
+    Internationalized, Internationalize, LabelValue,
+    ExtendedRef, I18nExtendedRef, ViewingDirection, Ref
+} from './Base';
+import Canvas, {CanvasBehavior} from './Canvas';
+import Collection, {CollectionBehavior} from './Collection';
+import Image, {AccessTier, Tiles, Features, Profile, Quality} from './Image';
+import Manifest, {ManifestBehavior} from './Manifest';
+import Resource from './Resource';
+import Service from './Service';
+
+export {
+    Annotation,
+    AnnotationCollection,
+    AnnotationPage,
+    AuthService,
+    Base,
+    Canvas,
+    Collection,
+    Image,
+    Manifest,
+    Resource,
+    Service,
+
+    TimeMode,
+    XYWH,
+    AuthTexts,
+    Internationalized,
+    Internationalize,
+    LabelValue,
+    ExtendedRef,
+    I18nExtendedRef,
+    ViewingDirection,
+    Ref,
+    CanvasBehavior,
+    CollectionBehavior,
+    AccessTier,
+    Tiles,
+    Features,
+    Profile,
+    Quality,
+    ManifestBehavior
+};
